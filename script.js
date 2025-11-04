8666cf42be35f6ce224adff2795b68cdc866864a
dafafa19871115-cmd <dafafa19871115@gmail.com>
2025-11-05 02:14:39 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    document.getElementById('message').innerText = 'You clicked the button! Hello from JavaScript!';
+    console.log('Button clicked!');
+});
